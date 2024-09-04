3df7579c469200082a5fffe2ccf549dd7122c4d6
Aktan Astarkulov <astarkulov.aktan@gmail.com>
2024-09-05 00:23:58 +0600

    feat: add burger btn

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,50 @@
-import './Header.css'
+import {useEffect, useState} from 'react';
+import './Header.css';
+import burgerBtn from '../../../public/images/burger-btn.svg';
+import krest from '../../../public/images/krest.svg'
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    useEffect(() => {
+        if (menuOpen) {
+            document.body.classList.add('no-scroll', 'blurred');
+        } else {
+            document.body.classList.remove('no-scroll', 'blurred');
+        }
+    }, [menuOpen]);
+
     return (
-        <div className="header">
-            <div>
-                <button className="nav-btn">About</button>
-                <button className="nav-btn">Categories</button>
-                <button className="nav-btn">How to join</button>
+        <>
+            <div className={`header`}>
+                <button className="burger" onClick={toggleMenu}>
+                    <img src={burgerBtn} alt="" />
+                </button>
+                <div className={`nav-menu`}>
+                    <button className="nav-btn">About</button>
+                    <button className="nav-btn">Categories</button>
+                    <button className="nav-btn">How to join</button>
+                </div>
+                <div>
+                    <button className="form-btn">Apply</button>
+                </div>
             </div>
-            <div>
-                <button className="form-btn">Apply</button>
-            </div>
-        </div>
-    )
-}
+            {menuOpen && (
+                <div className="mobile-menu">
+                    <button className="none-btn" onClick={toggleMenu}><img src={krest} alt=""/></button>
+                    <div className='mobile-buttons'>
+                        <button className="none-btn">About</button>
+                        <button className="none-btn">Categories</button>
+                        <button className="none-btn">How to join</button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
 
 export default Header;
